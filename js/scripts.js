57d2475766419d14b041e21e0a03e3b4b1003c21
Zachary Marston <zmarston@live.com>
2017-11-03 16:29:22 -0700

    add style

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 // back end
-function Pizza(toppingsArray, size, price){
+function Pizza(toppingsArray, size, cheese, price){
   this.toppings = toppingsArray;
   this.size = size;
+  this.cheese = cheese;
   this.price = 0;
   this.addPrice();
 }
 
 // totals price
 Pizza.prototype.addPrice = function () {
-  if (this.size === "small") {
+  if (this.size === "Small") {
     this.price += 10;
-  } else if (this.size === "medium") {
+  } else if (this.size === "Medium") {
     this.price += 15;
-  } else if (this.size === "large") {
+  } else if (this.size === "Large") {
     this.price += 20;
+  }
+  if (this.cheese === "No Cheese") {
+    this.price -=1;
   }
 
   var numberOfToppings = (this.toppings.length) * 2;
@@ -27,12 +31,13 @@ $(document).ready(function(){
   $("form#pizza").submit(function(event){
     event.preventDefault();
     var size = $("#size").val();
+    var cheese = $("#cheese").val();
     var toppings = [];
     $("input:checkbox[name=topping]:checked").each(function(){
       toppings.push($(this).val());
     });
-    var newPizza = new Pizza(toppings, size);
+    var newPizza = new Pizza(toppings, size, cheese);
     $("ul#order").text("");
-    $("ul#order").append("<li> Size: " + newPizza.size + "</li>" + "<li> Toppings: " + newPizza.toppings + "</li>" + "<li> Total: $" + newPizza.price + "</li>");
+    $("ul#order").append("<li> Size: " + newPizza.size + "</li>" + "<li> Cheese: " + newPizza.cheese + "</li>" + "<li> Toppings: " + newPizza.toppings + "</li>" + "<li> Total: $" + newPizza.price + "</li>");
   });
 });
